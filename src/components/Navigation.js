2ffe42e977da6b3d31374811f84e9345e21b22ae
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-07-27 21:58:46 +0500

    create components folder and added Navigation.js file

---
+++ src/components/Navigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import logo from '.././images/codecycle-logo.gif';
+import '.././App.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {/* position="static" */}
+      <AppBar id="Navi" >
+        <Toolbar variant="dense">
+          <img id="navimage" alt="logo animation" src={logo} /> &nbsp;&nbsp;
+          <Button id="navbutton" >Home</Button> &nbsp;&nbsp;
+          <Button id="navbutton" >Services</Button> &nbsp;&nbsp;
+          <Button id="navbutton" >About us</Button> &nbsp;&nbsp;
+          <Button id="navbutton" >Contact us</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
