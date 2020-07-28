201a4473ee47663ac5467e193df4fbd2e10d42b1
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-07-29 00:46:44 +0500

    updated App.css and LandingPage.js files

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '.././App.css';
 import landingright from '.././images/enterprise.gif';
 import mainimage from '.././images/newback.jpg';
+import aboutimage from '.././images/aboutus.png';
 import useWebAnimations, {fadeInUp} from "@wellyshen/use-web-animations";
 
 export default function LandingPage() {
@@ -17,13 +18,21 @@ export default function LandingPage() {
         <div>
             <div >
                 <img id="main" alt="main" src={mainimage} />
-            
-            <div id="landingleft">
-                <h1 ref={ref} style={{color: "white", textAlign: "left", paddingLeft: "220px", paddingTop: "70px"}}>Innovative and <br />intelligent technology <br />solutions and <br />services</h1>
+                <div id="landingleft">
+                    <h1 ref={ref} style={{color: "white", textAlign: "left", paddingLeft: "220px", paddingTop: "70px"}}>Innovative and <br />intelligent technology <br />solutions and <br />services</h1>
+                </div>
+                <div id="lright">
+                    <img id="landingright" alt="enterpr" src={landingright} />
+                </div>
             </div>
-            <div id="lright">
-                <img id="landingright" alt="enterpr" src={landingright} />
-            </div>
+            <div id="about">
+                <div id="labout">
+                    <img id="aboutleft" alt="aboutimag" src={aboutimage} />
+                </div>
+                <div id="aboutright">
+                    <h1 id="abhed">About</h1>
+                    <p style={{ color: "purple", textAlign: "left", paddingRight: "50px" }}>A highly specialized Management team with over 20 combined years of experience in the Healthcare industry with proven ability to optimally combine digital technology, skilled resources and efficient business processes to implement creative as well as proven market solutions to suit an ever-evolving market. Our forte has been to successfully tailor our solutions to support your needs whether it is in the space of Consulting, Product Management or Service Delivery</p>
+                </div>
 
             </div>
         </div>
