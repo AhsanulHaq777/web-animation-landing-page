5bc2aecfcbbf91284dfcdb816114ee8fbf42fcff
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-07-28 23:53:35 +0500

    installed animation.css library and updated LandingPage.js file

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,15 +2,24 @@ import React from 'react';
 import '.././App.css';
 import landingright from '.././images/enterprise.gif';
 import mainimage from '.././images/newback.jpg';
+import useWebAnimations, {fadeInUp} from "@wellyshen/use-web-animations";
 
 export default function LandingPage() {
+    const {keyframes, timing} = fadeInUp;
+    const {ref} = useWebAnimations({
+        keyframes,
+        timing: {
+            ...timing,
+            duration: timing.duration - 0.75,
+        },
+    });
     return(
         <div>
             <div >
                 <img id="main" alt="main" src={mainimage} />
             
             <div id="landingleft">
-                <h1 style={{color: "white", textAlign: "left", paddingLeft: "220px", paddingTop: "70px"}}>Innovative and <br />intelligent technology <br />solutions and <br />services</h1>
+                <h1 ref={ref} style={{color: "white", textAlign: "left", paddingLeft: "220px", paddingTop: "70px"}}>Innovative and <br />intelligent technology <br />solutions and <br />services</h1>
             </div>
             <div id="lright">
                 <img id="landingright" alt="enterpr" src={landingright} />
