e7ce0cb62d41bbe034bc0a0493760b5c3250ac1b
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-07-30 01:31:41 +0500

    added images, updated App.js and LandingPage.js files

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,10 +3,13 @@ import '.././App.css';
 import landingright from '.././images/enterprise.gif';
 import mainimage from '.././images/newback.jpg';
 import aboutimage from '.././images/aboutus.png';
-import useWebAnimations, {fadeInUp, zoomIn}  from "@wellyshen/use-web-animations";
+import webdesignimage from '.././images/websitedesign.gif';
+import seoimage from '.././images/Seoservice.gif';
+import marketingimage from '.././images/digitalmark.gif';
+import useWebAnimations, {fadeInUp, zoomIn, bounceIn}  from "@wellyshen/use-web-animations";
 
 export default function LandingPage() {
-    var mainref, aboutref;
+    var mainref, aboutref, serviceref1, serviceref2, serviceref3;
     {
         const {keyframes, timing} = fadeInUp;
         mainref = useWebAnimations({
@@ -28,6 +31,51 @@ export default function LandingPage() {
             }
         });
     }
+    
+    {        
+        const {keyframes, timing} = bounceIn;
+        serviceref1 = useWebAnimations({
+            keyframes,
+            timing: {
+                ...timing,
+                duration: timing.duration - 0.75,
+            }
+        });
+      
+    }
+    
+
+    {
+        const {keyframes, timing} = bounceIn;
+        serviceref2 = useWebAnimations({
+            keyframes,
+            timing: {
+                ...timing,
+                duration: timing.duration - 0.75,
+            }
+        });
+    }
+
+    {
+        const {keyframes, timing} = bounceIn;
+        serviceref3 = useWebAnimations({
+            keyframes,
+            timing: {
+                ...timing,
+                duration: timing.duration - 0.75,
+            }
+        });
+    }
+    const service1play = () => {
+        serviceref1.getAnimation().play();
+    }
+    const service2play = () => {
+        serviceref2.getAnimation().play();
+    }
+    const service3play = () => {
+        serviceref3.getAnimation().play();
+    }
+    
     return(
         <div>
             <div >
@@ -46,6 +94,21 @@ export default function LandingPage() {
                 <div id="aboutright" ref={aboutref.ref}>
                     <h1 id="abhed">About</h1>
                     <p style={{ color: "purple", textAlign: "left", paddingRight: "50px" }}>A highly specialized Management team with over 20 combined years of experience in the Healthcare industry with proven ability to optimally combine digital technology, skilled resources and efficient business processes to implement creative as well as proven market solutions to suit an ever-evolving market. Our forte has been to successfully tailor our solutions to support your needs whether it is in the space of Consulting, Product Management or Service Delivery</p>
+                </div>
+            </div>
+            <div id="services">
+                <h1 id="servhead">Services</h1>
+                <div onMouseOver={service1play} ref={serviceref1.ref} id="leftserv">
+                    <img id="servimages" alt="webdesign" src={webdesignimage} />
+                    <h3 id="servtitle">Website Designing</h3>
+                </div>
+                <div onMouseOver={service2play} ref={serviceref2.ref} id="middleserv">
+                    <img id="servimages" alt="seo" src={seoimage} />
+                    <h3 id="servtitle">Search Engine Optimization</h3>
+                </div>
+                <div onMouseOver={service3play} ref={serviceref3.ref} id="rightserv">
+                    <img id="servimages" alt="digital market" src={marketingimage} />
+                    <h3 id="servtitle">Digital Marketing</h3>
                 </div>
             </div>
         </div>
