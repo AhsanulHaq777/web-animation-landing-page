9bf7d7d4384207074f2b45a41d78e13e0f59c878
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-07-31 01:53:34 +0500

    installed animation-on-scroll library, updated App.css, LandingPage.js, package.json and yarn.lock files

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '.././App.css';
 import landingright from '.././images/enterprise.gif';
 import mainimage from '.././images/newback.jpg';
@@ -7,10 +7,17 @@ import webdesignimage from '.././images/websitedesign.gif';
 import seoimage from '.././images/Seoservice.gif';
 import marketingimage from '.././images/digitalmark.gif';
 import workimage from '.././images/preview.gif';
-import useWebAnimations, {fadeInUp, zoomIn, bounceIn}  from "@wellyshen/use-web-animations";
+import useWebAnimations, {fadeInUp, bounceIn}  from "@wellyshen/use-web-animations";
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 export default function LandingPage() {
-    var mainref, aboutref, serviceref1, serviceref2, serviceref3;
+
+    useEffect(()=>{
+        Aos.init({duration: 2000});
+    },[]);
+
+    var mainref, serviceref1, serviceref2, serviceref3;
     {
         const {keyframes, timing} = fadeInUp;
         mainref = useWebAnimations({
@@ -21,17 +28,17 @@ export default function LandingPage() {
             },
         });
     }
-    {
-        const {keyframes, timing} = zoomIn;
-        aboutref = useWebAnimations({
-            keyframes,
-            timing: {
-                ...timing,
-                duration: timing.duration - 0.75,
+    // {
+    //     const {keyframes, timing} = zoomIn;
+    //     aboutref = useWebAnimations({
+    //         keyframes,
+    //         timing: {
+    //             ...timing,
+    //             duration: timing.duration - 0.75,
                 
-            }
-        });
-    }
+    //         }
+    //     });
+    // }
     
     {        
         const {keyframes, timing} = bounceIn;
@@ -83,7 +90,7 @@ export default function LandingPage() {
                 <div id="landingleft">
                     <h1 ref={mainref.ref} style={{color: "white", textAlign: "left", paddingLeft: "220px", paddingTop: "70px"}}>Innovative and <br />intelligent technology <br />solutions and <br />services</h1>
                 </div>
-                <div id="lright">
+                <div data-aos="fade-left" id="lright">
                     <img id="landingright" alt="enterpr" src={landingright} />
                 </div>
             </div>
@@ -91,27 +98,27 @@ export default function LandingPage() {
                 <div id="labout">
                     <img id="aboutleft" alt="aboutimag" src={aboutimage} />
                 </div>
-                <div id="aboutright" ref={aboutref.ref}>
+                <div id="aboutright" data-aos="zoom-in" >
                     <h1 id="abhed">About</h1>
                     <p style={{ color: "purple", textAlign: "left", paddingRight: "50px" }}>A highly specialized Management team with over 20 combined years of experience in the Healthcare industry with proven ability to optimally combine digital technology, skilled resources and efficient business processes to implement creative as well as proven market solutions to suit an ever-evolving market. Our forte has been to successfully tailor our solutions to support your needs whether it is in the space of Consulting, Product Management or Service Delivery</p>
                 </div>
             </div>
             <div id="services">
                 <h1 id="servhead">Services</h1>
-                <div onMouseOver={service1play} ref={serviceref1.ref} id="leftserv">
-                    <img id="servimages" alt="webdesign" src={webdesignimage} />
+                <div data-aos="zoom-out-right" onMouseOver={service1play}  id="leftserv">
+                    <img ref={serviceref1.ref} id="servimages" alt="webdesign" src={webdesignimage} />
                     <h3 id="servtitle">Website Designing</h3>
                 </div>
-                <div onMouseOver={service2play} ref={serviceref2.ref} id="middleserv">
-                    <img id="servimages" alt="seo" src={seoimage} />
+                <div data-aos="zoom-out-up" onMouseOver={service2play}  id="middleserv">
+                    <img ref={serviceref2.ref} id="servimages" alt="seo" src={seoimage} />
                     <h3 id="servtitle">Search Engine Optimization</h3>
                 </div>
-                <div onMouseOver={service3play} ref={serviceref3.ref} id="rightserv">
-                    <img id="servimages" alt="digital market" src={marketingimage} />
+                <div data-aos="zoom-out-left" onMouseOver={service3play}  id="rightserv">
+                    <img ref={serviceref3.ref} id="servimages" alt="digital market" src={marketingimage} />
                     <h3 id="servtitle">Digital Marketing</h3>
                 </div>
             </div>
-            <div id="footerimg" >
+            <div data-aos="flip-left" id="footerimg" >
                 <img id="workimage" alt="workhome" src={workimage} />
             </div>
             
